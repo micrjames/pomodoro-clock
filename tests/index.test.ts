2570370699ccf66eef42c6b3caa5321182a7e60b
Michael James <micrjames921@gmail.com>
2024-12-09 20:13:45 -0700

    setup project code and tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,26 +1,24 @@
-const {} = require("");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+import path from "path";
+import fs from "fs";
 
-describe("", () => {
+describe("A Pomodoro Clock", () => {
+   let dom: typeof JSDOM;
+   let document: Document;
+   let body: HTMLElement
    beforeAll(() => {
+	  const html = fs.readFileSync(path.resolve(__dirname, "..", "src", "index.html"), "utf8"); 
+	  dom = new JSDOM(html, { runScripts: 'dangerously' });
+	  document = dom.window.document;
+	  body = document.body;
+	  const bodyEls = body.children;
+	  for(const bodyEl of bodyEls)
+		 console.log(bodyEl);
    });
-   describe("", () => {
-	  test.todo("", () => {
-	  });
-	  test.todo("", () => {
-	  });
-	  test.todo("", () => {
-	  });
-	  test.todo("", () => {
-	  });
-   });
-   describe("", () => {
-	  test.todo("", () => {
-	  });
-	  test.todo("", () => {
-	  });
-	  test.todo("", () => {
-	  });
-	  test.todo("", () => {
-	  });
+   describe("That Has No Timer", () => {
+	  test.todo("Should Exist.");
+	  test.todo("Should Have the Given Layout.");
+	  test.todo("Should Show a Start Time.");
    });
 });
